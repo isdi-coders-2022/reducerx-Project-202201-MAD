868cbd6222fe0b6da1af9249669073de9e70ab50
Mel Escolano <amalia.escolano@gmail.com>
2022-02-14 16:12:36 +0100

    Tested Footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { Footer } from './components/footer';
 function App() {
     return (
         <div className="App">
-            <Footer />
+            <footer id="footer">
+                <Footer />
+            </footer>
         </div>
     );
 }
-
 export default App;
---
+++ src/App.spec.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders App', () => {
+    render(<App />);
+    const linkElement = screen.getByText(
+        /Shaked and mixed by ReducerX with love/i
+    );
+    expect(linkElement).toBeInTheDocument();
+});
