1f4bb1f0d11d20e02ed44797c81f47fc59f4d8af
Íñigo Paños <inigopanos@gmail.com>
2022-02-15 12:05:33 +0100

    Added Header component .css, restructured files and modified App.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
         <>
-            <img src="/assets/cocktailCopa.png" alt="Cocktail logo neon cup" />
+            {/* <img src="/assets/cocktailCopa.png" alt="Cocktail logo neon cup" /> */}
             <address>
                 <p>Shaked and mixed by ReducerX with love ❤️╰(*°▽°*)╯❤️</p>
             </address>
---
+++ src/components/header/header.js
@@ -0,0 +1,27 @@
+import './header-style.scss';
+
+export function Header() {
+    return (
+        <header className="header">
+            <h1 className="header-h1">Cocktails!</h1>
+            <div className="header-images">
+                <img
+                    className="header-img"
+                    src="/assets/cocktail3.png"
+                    alt="Neon cocktail logo"
+                />
+                <img
+                    className="header-menu-icon"
+                    src="/assets/menu.png"
+                    alt="Hamburgesa de menu"
+                />
+                <nav className="desktop-nav">
+                    <ul className="desktop-nav__menu">
+                        <li>Favorites</li>
+                        <li>Home</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
