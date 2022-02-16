1b62b64cc150e39d2bf6a3e928194772e98a36cb
Mel Escolano <amalia.escolano@gmail.com>
2022-02-16 16:52:22 +0100

    Update cocktail-fav and test-cocktail-fav files

--- src/components/cocktail-list-fav/cocktail-list-fav.js
+++ src/components/cocktail-list-fav/cocktail-list-fav.js
@@ -11,20 +11,41 @@ export function CocktailListFav() {
             {store.favorites ? (
                 <main>
                     <h3>My Cocktails</h3>
-                    <ul className="cocktail-list-fav">
+                    <ul className="cocktail-list">
                         {store.favorites.map((item) => (
                             <li
                                 key={item.idDrink}
-                                className="cocktail-list-fav__item"
+                                className="cocktail-list__name"
                             >
-                                <img
-                                    src={item.strDrinkThumb}
-                                    alt="Favorite Cocktail"
-                                />
-                                <p> {item.strDrink}</p>
+                                <div>
+                                    <img
+                                        src={item.strDrinkThumb}
+                                        alt="Cocktail"
+                                    />
+                                    <p className="cocktail-title__mobile">
+                                        {item.strDrink}
+                                    </p>
+                                </div>
+                                <p className="cocktail-title__desktop">
+                                    {item.strDrink}
+                                </p>
                             </li>
                         ))}
                     </ul>
+                    <div className="cocktails-navigation">
+                        <button
+                            type="button"
+                            className="cocktails-navigation__previous"
+                        >
+                            Previous
+                        </button>
+                        <button
+                            type="button"
+                            className="cocktails-navigation__next"
+                        >
+                            Next
+                        </button>
+                    </div>
                 </main>
             ) : (
                 <p>Loading</p>
