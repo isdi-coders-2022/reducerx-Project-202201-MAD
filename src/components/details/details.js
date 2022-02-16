399b5fd587d4993297c0d8d86d909de2099d7e91
Íñigo Paños <inigopanos@gmail.com>
2022-02-16 16:47:33 +0100

    Fixed details API call

--- src/components/details/details.js
+++ src/components/details/details.js
@@ -1,17 +1,23 @@
+import { useState, useEffect } from 'react';
 import * as api from '../../services/api';
 
-export async function Details() {
+export function Details() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const cocktailId = urlParams.get('id');
     const detailsURL = `https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=${cocktailId}`;
-    let dataTry = [];
-    const { data } = await api.getDetails(detailsURL);
-    dataTry = data;
-    // console.log(data.drinks);
-    return (
-        <div>
-            {dataTry.drinks ? <p>{dataTry.drinks.idDrink}</p> : <p>Loading</p>}
-        </div>
-    );
+
+    const [state, setState] = useState({});
+
+    useEffect(() => {
+        api.getDetails(detailsURL)
+            .then((resp) => {
+                console.log(resp);
+                setState(resp.data.drinks[0]);
+            })
+            .catch((error) => console.log(error));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return <div>{state ? <p>{state.idDrink}</p> : <p>Loading</p>}</div>;
 }
