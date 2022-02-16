c0f6ee98ca7f016391f4131183db4b1df4c377d9
Mel Escolano <amalia.escolano@gmail.com>
2022-02-16 12:20:07 +0100

    Fixed conflicts with unreacheable module in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import { CocktailList } from './components/cocktail-list/cocktail-list';
-
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
-import { CocktailListFav } from './components/cocktail-list-fav/cocktail-list-fav';
 
 function App() {
     const menuOptions = [
@@ -18,7 +16,6 @@ function App() {
             <Routes>
                 <Route path="/" element={<CocktailList />} />
                 <Route path="/home" element={<CocktailList />} />
-                <Route path="/favorites" element={<CocktailListFav />} />
             </Routes>
 
             <Footer />
