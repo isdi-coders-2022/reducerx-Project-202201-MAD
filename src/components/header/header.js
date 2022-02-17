53abf4c6338499941b213d9f6881f7cd962cb139
Íñigo Paños <inigopanos@gmail.com>
2022-02-17 12:35:54 +0100

    Added auth logout

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,13 @@
 import './header-style.scss';
+import { useAuth0 } from '@auth0/auth0-react';
 import { Menu } from './menu';
 import { LoginButton } from '../auth0/LoginButton';
+import { LogoutButton } from '../auth0/LogOut';
+import { Profile } from '../auth0/UserInfo';
 
 export function Header({ menuOptions }) {
+    const { isAuthenticated } = useAuth0();
+
     return (
         <header className="header" id="headerID">
             <h1 className="header-h1">Cocktails!</h1>
@@ -17,7 +22,14 @@ export function Header({ menuOptions }) {
                     src="/assets/menu.png"
                     alt="Hamburgesa de menu"
                 />
-                <LoginButton />
+                {isAuthenticated ? (
+                    <>
+                        <LogoutButton /> <Profile />
+                    </>
+                ) : (
+                    <LoginButton />
+                )}
+
                 {/* <nav className="desktop-nav">
                     <ul className="desktop-nav__menu">
                         <li className="desktop-home">Home</li>
