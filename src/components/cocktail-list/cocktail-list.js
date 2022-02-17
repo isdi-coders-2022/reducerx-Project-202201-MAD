5b9a87a40da6b1d7a4481d490407053aa27d6ba5
Carolina Ros <rosdelreal@gmail.com>
2022-02-17 09:23:29 +0100

    Changed pagination cocktails

--- src/components/cocktail-list/cocktail-list.js
+++ src/components/cocktail-list/cocktail-list.js
@@ -1,48 +1,60 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Context } from '../../context/context';
 import './cocktail-list.scss';
 
 export function CocktailList() {
     const { store } = useContext(Context);
+    const [indexMarker, setindexMarker] = useState(0);
+
+    function handleOnClick(index) {}
 
     return (
         <div>
             {store.cocktails ? (
                 <main>
                     <ul className="cocktail-list">
-                        {store.cocktails.map((item) => (
-                            <li
-                                key={item.idDrink}
-                                className="cocktail-list__name"
-                            >
-                                <Link to={`/details?id=${item.idDrink}`}>
-                                    <div>
-                                        <img
-                                            src={item.strDrinkThumb}
-                                            alt="Cocktail"
-                                        />
+                        {store.cocktails.map(
+                            (item, index) =>
+                                index >= indexMarker &&
+                                index < indexMarker + 8 && (
+                                    <li
+                                        key={item.idDrink}
+                                        className="cocktail-list__name"
+                                    >
+                                        <Link
+                                            to={`/details?id=${item.idDrink}`}
+                                        >
+                                            <div>
+                                                <img
+                                                    src={item.strDrinkThumb}
+                                                    alt="Cocktail"
+                                                />
 
-                                        <p className="cocktail-title__mobile">
-                                            {item.strDrink}
-                                        </p>
-                                    </div>
-                                    <p className="cocktail-title__desktop">
-                                        {item.strDrink}
-                                    </p>
-                                </Link>
-                            </li>
-                        ))}
+                                                <p className="cocktail-title__mobile">
+                                                    {item.strDrink}
+                                                </p>
+                                            </div>
+                                            <p className="cocktail-title__desktop">
+                                                {item.strDrink}
+                                            </p>
+                                        </Link>
+                                    </li>
+                                )
+                        )}
                     </ul>
+
                     <div className="cocktails-navigation">
                         <button
+                            onClick={handleOnClick}
                             type="button"
                             className="cocktails-navigation__previous"
                         >
                             Previous
                         </button>
                         <button
+                            onClick={handleOnClick}
                             type="button"
                             className="cocktails-navigation__next"
                         >
