a37a28e03cd5072775b4d018d78e4966867bb822
Carolina Ros <rosdelreal@gmail.com>
2022-02-17 15:26:45 +0100

    Changed burger icon

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,18 @@
 import './header-style.scss';
+import { useState } from 'react';
+import { slide as MenuSlide } from 'react-burger-menu';
+import { Link } from 'react-router-dom';
+import { Fade as Hamburger } from 'hamburger-react';
 import { Menu } from './menu';
 
 export function Header({ menuOptions }) {
+    const [isOpen, setOpen] = useState(false);
+
+    // const toggledBurger = () => {
+    //     console.log(isOpen);
+    //     setOpen(!isOpen);
+    // };
+
     return (
         <header className="header" id="headerID">
             <h1 className="header-h1">Cocktails!</h1>
@@ -11,17 +22,47 @@ export function Header({ menuOptions }) {
                     src="/assets/cocktail3.png"
                     alt="Neon cocktail logo"
                 />
-                <img
-                    className="header-menu-icon"
-                    src="/assets/menu.png"
-                    alt="Hamburgesa de menu"
-                />
-                {/* <nav className="desktop-nav">
-                    <ul className="desktop-nav__menu">
-                        <li className="desktop-home">Home</li>
-                        <li className="desktop-favourites">Favourites</li>
-                    </ul>
-                </nav> */}
+                <div className="header-menu-icon">
+                    <Hamburger
+                        toggled={isOpen}
+                        onToggle={() => setOpen(!isOpen)}
+                    />
+                    {/* <img
+                        className="header-menu-icon"
+                        src="/assets/menu.png"
+                        alt="Hamburgesa de menu"
+                    /> */}
+                    {/* <MenuSlide
+                        right
+                        noOverlay
+                        width="200px"
+                        height="50%"
+                        onClick={toggleBurger}
+                    >
+                        {menuOptions.map((item) => (
+                            <li className="menuitem" key={item.path}>
+                                <Link
+                                    className="menu__item-link"
+                                    to={item.path}
+                                >
+                                    {item.label}
+                                </Link>
+                            </li>
+                        ))}
+                    </MenuSlide> */}
+                    {isOpen &&
+                        menuOptions.map((item) => (
+                            <li className="menuitem" key={item.path}>
+                                <Link
+                                    className="menu__item-link"
+                                    to={item.path}
+                                >
+                                    {item.label}
+                                </Link>
+                            </li>
+                        ))}
+                </div>
+
                 <Menu menuOptions={menuOptions} />
             </div>
         </header>
